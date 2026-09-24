b5581db077148937c344fd4f2c34cecd21ec921a
Vén Dávid Zsolt <ven.david@akk.lan>
2026-09-24 14:57:57 +0200

    meret keveres

--- src/test/Tesztek.java
+++ src/test/Tesztek.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package test;
 
-/**
- *
- * @author ammer.mark
- */
+import kartyacuccoop.Pakli;
+import kartyacuccoop.Program;
+
+
 public class Tesztek {
     
-}
+    private static void pakliMeret(){
+        Pakli pakli = new Pakli();
+        String[] kartyak = pakli.getKartyak();
+        
+        if (kartyak != null && kartyak.length == 22) {
+            System.out.println("pakliMeret: SIKERES");
+        } else {
+            System.out.println("pakliMeret: HIBA! A tömb nem 22 elemű vagy null.");
+        }
+    }
+        public static void testOszlopKeveres() {
+        Pakli pakli = new Pakli();
+        pakli.kever(1);
+        String[] kartyakUtana = pakli.getKartyak();
+        
+        assert kartyakUtana.length == 22 : "keveres utan se valtozik a merete.";
+        System.out.println("testOszlopKeveres: SIKERES");
+    }
+    }
+    
+
+    
+
+    
+
